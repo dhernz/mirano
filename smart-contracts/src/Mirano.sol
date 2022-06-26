47813b34a38d73ae7b18d0bfd813a0483259ee4e
Yasmeen Roumie <yasmeen@hook.xyz>
2022-06-26 07:44:42 -0400

    Call swivel initiate function with order

--- smart-contracts/src/Mirano.sol
+++ smart-contracts/src/Mirano.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.9;
 
+import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "./integrations/swivel/Interfaces.sol";
+import "./integrations/swivel/Hash.sol";
+
 enum Category {
     Supermarket,
     Restaurant,
@@ -8,18 +12,18 @@ enum Category {
     Utility
 }
 
-contract Mirano is ERC20 {
+contract Mirano {
     // Approved spending categories for a user
     mapping(address => Category[]) approvedCategories;
 
-    // Balance for a user
     mapping(address => uint256) balances;
+
+    // Swivel Polygon Mumbai testnet contract: 0xA772F08a5B11d85eB2a95F6c923b86AF1a8fb614
 
     function setApprovedCategories(
         address addr,
         Category[] storage categoriesToApprove
     ) private {
-        // _setApprovedCategories(addr, approvedCategories, categoriesToAdd);
         approvedCategories[addr] = categoriesToApprove;
     }
 
@@ -35,27 +39,36 @@ contract Mirano is ERC20 {
         Category[] storage categoriesToApprove
     ) public payable {
         setApprovedCategories(addr, categoriesToApprove);
+        ERC20.transfer(addr, msg.value);
     }
 
     function sendForSavings(
         address addr,
         uint256 amount,
         uint256 withdrawalAllowedTime
-    ) {}
+    ) {
+        // initialize an empty order struct and then update it
+        Hash.Order memory order;
+        order.maker = addr;
 
-    function depositToLendingPool(uint256 amount) private {
-        // After deposit msg.sender receives the aToken
-        // IWETHGateway(wethGatewayAddr).depositETH{value: amount}(
-        //     poolAddr,
-        //     address(this),
-        //     0
-        // );
-    }
+        // TST token on Polygon mumbai testnet
+        order.underlying = "0x2d7882beDcbfDDce29Ba99965dd3cdF7fcB10A1e"; // address of ERC20 token
+        order.vault = true;
+        order.exit = false;
+        order.principal = 0;
+        order.premium = amount;
+        order.maturity = 0; // The maturity date of the market in seconds since epoch.
+        order.expiry = block.timestamp; // Timestamp marking this order's expiration
 
-    function withdrawFromLendingPool(uint256 amount, address to) private {
-        // calling contract should have enough credit limit
-        // IAToken(ATokenAddress).approve(wethGatewayAddr, amount);
-        // Withdrawn amount will be send to to address
-        // IWETHGateway(wethGatewayAddr).withdrawETH(poolAddr, amount, to);
+        // Swivel Polygon Mumbai testnet contract
+        address contractAddress = 0xA772F08a5B11d85eB2a95F6c923b86AF1a8fb614;
+        bytes32 eip712 = Hash.domain(
+            Swivel.NAME,
+            Swivel.VERSION,
+            137,
+            contractAddress
+        );
+
+        Swivel.initiate(Hash.Order[order], 0, 0);
     }
 }
